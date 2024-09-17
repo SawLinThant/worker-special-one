bc0088bb646093defe6e70b7b44a8d9ddf2d38af
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-09-17 15:40:36 +0630

    phone number changing

--- src/modules/common/components/footer/index.tsx
+++ src/modules/common/components/footer/index.tsx
@@ -67,7 +67,7 @@ const Footer = () => {
             <li className="mt-4">
               <a
                 className="flex flex-row items-center gap-4"
-                href="https://wa.me/9595055517"
+                href="https://wa.me/95985055517"
               >
                 <FaWhatsapp color="white" size={15} />
                 <p>Whatsapp</p>
